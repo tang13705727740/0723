6df3c16d32db25484a1bfcfbd2c0e03ea302b27c
tang13705727740 <57855219+tang13705727740@users.noreply.github.com>
2020-07-24 15:46:53 +0800

    Add files via upload
    新增bbblist.js

---
+++ src/views/ProductLists/bbblist.js
@@ -0,0 +1,91 @@
+const bbblist = [
+  {
+    url: require('../../assets/phones/1.png'),
+    name: 'Redmi9'
+  },
+  {
+    url: require('../../assets/phones/2.png'),
+    name: 'Redmi8'
+  },
+  {
+    url: require('../../assets/phones/3.png'),
+    name: 'Redmi7'
+  },
+  {
+    url: require('../../assets/phones/4.png'),
+    name: 'Redmi6'
+  },
+  {
+    url: require('../../assets/phones/5.png'),
+    name: 'Redmi5'
+  },
+  {
+    url: require('../../assets/phones/6.png'),
+    name: 'Redmi4'
+  },
+  {
+    url: require('../../assets/phones/7.png'),
+    name: 'Redmi3'
+  },
+  {
+    url: require('../../assets/phones/8.jpg'),
+    name: 'Redmi2'
+  },
+  {
+    url: require('../../assets/phones/10.jpg'),
+    name: 'Redmi1'
+  },
+  {
+    url: require('../../assets/phones/11.jpg'),
+    name: 'Redmi0'
+  },
+  {
+    url: require('../../assets/phones/12.jpg'),
+    name: 'Redmi-1'
+  },
+  {
+    url: require('../../assets/phones/13.jpg'),
+    name: 'Redmi-2'
+  },
+  {
+    url: require('../../assets/phones/14.jpg'),
+    name: 'Redmi-3'
+  },
+  {
+    url: require('../../assets/phones/15.jpg'),
+    name: 'Redmi-4'
+  },
+  {
+    url: require('../../assets/phones/16.webp'),
+    name: 'Redmi-5'
+  },
+  {
+    url: require('../../assets/phones/17.webp'),
+    name: 'Redmi-6'
+  },
+  {
+    url: require('../../assets/phones/18.webp'),
+    name: 'Redmi-7'
+  },
+  {
+    url: require('../../assets/phones/19.webp'),
+    name: 'Redmi-8'
+  },
+  {
+    url: require('../../assets/phones/20.webp'),
+    name: 'Redmi-9'
+  },
+  {
+    url: require('../../assets/phones/21.webp'),
+    name: 'Redmi-10'
+  },
+  {
+    url: require('../../assets/phones/17.webp'),
+    name: 'Redmi-11'
+  },
+  {
+    url: require('../../assets/phones/18.webp'),
+    name: 'Redmi-12'
+  }
+]
+export default bbblist
