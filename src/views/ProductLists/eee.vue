4863e379f48dedf3bc879afa26e300aa947ffb21
tang13705727740 <57855219+tang13705727740@users.noreply.github.com>
2020-07-24 15:52:35 +0800

    新增eee.vue

---
+++ src/views/ProductLists/eee.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="allbox">
+    <div class="eachli" v-for="item in aaalist" :key="item.name">
+      <img :src="item.url" alt="" />
+      <span>{{ item.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import aaalist from '../ProductLists/aaalist'
+export default {
+  data() {
+    return {
+      aaalist
+    }
+  }
+}
+</script>
+
+<style></style>
